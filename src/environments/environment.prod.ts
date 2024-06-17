472d87c00ac2223432929bad8cb83f536446cd7c
Mark <markus.anthony.garcia@gmail.com>
2024-06-16 23:13:48 -0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.nightowlvibes.app',
-  googleClientKey: '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',
+  googleClientKey: '257251695950-e3ums9ol02arr96uf1kjphakp607i59f.apps.googleusercontent.com',
   facebookClientKey: '421946357352421',
 };
