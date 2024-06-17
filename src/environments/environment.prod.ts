9a0ba72edb4309378a97b5c65fc7a5a3e125193e
Mark <markus.anthony.garcia@gmail.com>
2024-06-17 00:13:32 -0500

    update api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.nightowlvibes.app',
+  apiUrl: 'https://api.nocturne-app.com',
   googleClientKey: '257251695950-e3ums9ol02arr96uf1kjphakp607i59f.apps.googleusercontent.com',
   facebookClientKey: '3136069826530480',
 };
